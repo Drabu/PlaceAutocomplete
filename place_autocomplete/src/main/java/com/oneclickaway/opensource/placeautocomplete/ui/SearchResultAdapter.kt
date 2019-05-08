86040638de806dce28d172fa7795fb88bfd6abf5
Burhan ud din Drabu <burhan.drabu@gmail.com>
2019-05-08 18:13:52 +0530

    testRelease removed db android fixed adapter

--- place_autocomplete/src/main/java/com/oneclickaway/opensource/placeautocomplete/ui/SearchResultAdapter.kt
+++ place_autocomplete/src/main/java/com/oneclickaway/opensource/placeautocomplete/ui/SearchResultAdapter.kt
@@ -3,13 +3,12 @@ package com.oneclickaway.opensource.placeautocomplete.ui
 import android.annotation.SuppressLint
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import com.jakewharton.rxbinding2.view.RxView
+import com.oneclickaway.opensource.placeautocomplete.R
 import com.oneclickaway.opensource.placeautocomplete.api.bean.places_response.PredictionsItem
-import com.oneclickaway.opensource.placeautocomplete.databinding.SearchResultRowBinding
 import com.oneclickaway.opensource.placeautocomplete.interfaces.PlaceClickListerner
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 /** @author @buren ---> {adapter to set result views in row}*/
@@ -21,18 +20,20 @@ class SearchResultAdapter(
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder =
         ViewHolder(
-            binding = SearchResultRowBinding.inflate(LayoutInflater.from(p0.context), p0, false),
-            listOfCandidatesItem = listOfCandidatesItem,
+
+           listOfCandidatesItem = listOfCandidatesItem,
+           binding =  LayoutInflater.from(p0.context).inflate(R.layout.search_result_row,   p0,  false),
             placeClickListerner = placeClickListerner
+
         )
 
     override fun getItemCount(): Int = listOfCandidatesItem!!.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-        holder.binding.placeTitleTV.text = listOfCandidatesItem?.get(position)?.structuredFormatting?.mainText
+        /*holder.binding.placeTitleTV.text = listOfCandidatesItem?.get(position)?.structuredFormatting?.mainText
         holder.binding.placeFormattedAddressTV.text =
-            listOfCandidatesItem?.get(position)?.structuredFormatting?.secondaryText
+            listOfCandidatesItem?.get(position)?.structuredFormatting?.secondaryText*/
 
     }
 
@@ -47,9 +48,9 @@ class SearchResultAdapter(
     /** @author @buren ---> {holds row vew}*/
     class ViewHolder(
         var listOfCandidatesItem: List<PredictionsItem?>?,
-        var binding: SearchResultRowBinding,
+        var binding: View,
         var placeClickListerner: PlaceClickListerner
-    ) : RecyclerView.ViewHolder(binding.root) {
+    ) : RecyclerView.ViewHolder(binding) {
 
         init {
             setItemClickListener()
@@ -57,7 +58,7 @@ class SearchResultAdapter(
 
         @SuppressLint("CheckResult")
         private fun setItemClickListener() {
-            RxView.clicks(binding.root)
+            RxView.clicks(binding)
                 .throttleFirst(700, TimeUnit.MILLISECONDS)
                 .subscribe {
                     placeClickListerner.onPlaceClicked(listOfCandidatesItem?.get(adapterPosition))
